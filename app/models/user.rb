cc9147b2883cc84240bcf32130679ea673f0c19c
H-Ramalan <talktoramalan@gmail.com>
2023-10-18 01:12:38 -0700

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy, class_name: 'Recipe'
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, dependent: :destroy
-
 end
