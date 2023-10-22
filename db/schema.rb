6b6fa360eb388f3557534d3c343310cacefc8364
H-Ramalan <talktoramalan@gmail.com>
2023-10-21 21:15:21 -0700

    Added Food_id references

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_103254) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_22_040540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name"
     t.string "measurement_unit"
     t.integer "price"
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.bigint "recipe_id", null: false
-    t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
@@ -40,10 +40,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_103254) do
     t.integer "preparation_time"
     t.integer "cooking_time"
     t.text "description"
-    t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "public"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
